376e72cc50514dd62fbe2068c9ff30c7fe85f8fe
Ivan Driuk <veytarn@gmail.com>
2018-01-21 15:58:49 +0200

    move app component to app folder, add react-redux, redux, react-router-dom, redux-promise, axios, redux-form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise'
+
 import './index.css';
-import App from './App';
+
+import reducers from './reducers';
+
+import App from './components/App/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
+
 registerServiceWorker();

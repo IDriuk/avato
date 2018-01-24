ee8e4aa85631e7695f9a5820184c0b51294c9440
Ivan Driuk <veytarn@gmail.com>
2018-01-24 20:50:41 +0200

    add header services

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Fragment>
+
+        <div className="header-root-32BPK header-root_no-bottom-margin-7gPk3">
+
+          <div className="header-services-2XcAz header-services">
+            <div className="header-services-inner-37pha header-clearfix-2C_lO">
+              <ul className="header-list-1QTjc header-services-nav-HNIx_ header-clearfix-2C_lO">
+                <li className="header-services-nav-item-22BQy">
+                  <a className="header-link-1PHO2 header-services-nav-link-1bw0t">Объявления</a>
+                </li>
+                <li className="header-services-nav-item-22BQy">
+                  <a className="header-link-1PHO2 header-services-nav-link-1bw0t">Магазины</a>
+                </li>
+                <li className="header-services-nav-item-22BQy">
+                  <a className="header-link-1PHO2 header-services-nav-link-1bw0t">Помощь</a>
+                </li>
+              </ul>
+              <div className="header-services-menu-HMZRN">
+                <div className="header-services-menu-item_favorites-z9bV6">
+                  <a className="header-services-menu-link-IZnN3">
+                    <span className="header-services-menu-icon-3L63I">
+                      <svg width="21" height="21" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke="#0AF" strokeWidth="7.2%" fill="#fff" d="M6.162 16.876c-.325.194-.45.143-.363-.22l.993-4.588c.038-.153.02-.212-.102-.315l-3.517-3.16c-.289-.243-.206-.317.173-.346l4.692-.566c.16-.012.248-.076.31-.221l1.94-4.202c.145-.344.256-.344.402 0l1.968 4.202c.061.145.15.209.31.221l4.677.566c.379.03.476.06.187.303l-3.517 3.203c-.122.103-.14.162-.102.315l1.042 4.581c.088.363-.102.442-.426.248l-4.155-2.293c-.137-.082-.206-.082-.343 0l-4.17 2.272z">
+                        </path>
+                      </svg>
+                    </span>
+                  </a>
+                </div>
+                <div className=" header-services-menu-item_username-ZhPzt">
+                  <a className="header-services-menu-link-IZnN3">
+                    Вход и регистрация
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+
+      </Fragment>
     );
   }
 }

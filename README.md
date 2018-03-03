3e0be0bec1d47807bbe5cc832367f64f9bc1512e
Ivan Driuk <veytarn@gmail.com>
2018-03-03 12:25:01 +0200

    remove redux from project, update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## Example of my work with HTML, CSS, Javascript, React.js, Redux... bla, bla, bla ... and Github :)
+## Example of my work with HTML, CSS, Javascript and React.js :)
+### Frontend for main page of popular russian internet shop.
+#### [docs](https://github.com/IDriuk/avato/blob/master/create_react_app_readme.md)
--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import promise from 'redux-promise'
-
 import './index.css';
-
-import reducers from './reducers';
 
 import App from './components/App/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+  </BrowserRouter>,
   document.getElementById('root'));
 
 registerServiceWorker();
